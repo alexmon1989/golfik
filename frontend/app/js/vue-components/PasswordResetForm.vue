e922121e3165679647dd3df5c3292e3933e123da
Alexandr Monastyrtesky <alex.mon1989@gmail.com>
2018-02-23 14:48:53 +0200

    refactoring: оптимизация формы сброса пароля.

--- frontend/app/js/vue-components/PasswordResetForm.vue
+++ frontend/app/js/vue-components/PasswordResetForm.vue
@@ -3,10 +3,10 @@
         <form @submit.prevent="validate" id="password-reset-form" class="g-py-15" action="" method="post">
           <div v-html="csrfField"></div>
           <p>Введите свой адрес электронной почты и мы вышлем вам новый пароль.</p>
-          <div :class="{'u-has-error-v1': errors.has('email') || emailServerErrors.length > 0 }" class="mb-4">
+          <div :class="{'u-has-error-v1': errors.has('email') || (emailServerErrors && emailServerErrors.length > 0) }" class="mb-4">
             <input v-model="email" @keypress="hideServerErrors()" data-vv-as="Email" v-validate="'required|email'" name="email" class="form-control g-color-black g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15" type="email" placeholder="johndoe@gmail.com">
             <small v-if="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</small>
-            <small v-if="showServerErrors && emailServerErrors.length > 0" class="form-control-feedback">{{ emailServerErrors }}</small>
+            <small v-if="showServerErrors && emailServerErrors && emailServerErrors.length > 0" class="form-control-feedback">{{ emailServerErrors }}</small>
           </div>
 
           <div class="mb-4">
